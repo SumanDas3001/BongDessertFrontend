60aaeb06ee5981f83e70821a59006eafbb83da84
SumanDas3001 <sumandas3001@gmail.com>
2022-07-07 16:53:12 +0530

    Integrate infinite loading

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,53 +1,102 @@
-import React from "react";
 import styled from "styled-components";
 import product1 from "../assets/product1.jpg";
 import product2 from "../assets/product2.jpg";
 import product3 from "../assets/product3.jpg";
 import product4 from "../assets/product4.jpg";
+import product5 from "../assets/product5.jpg";
+import product6 from "../assets/product6.jpg";
+import product7 from "../assets/product7.jpg";
+import product8 from "../assets/product8.jpg";
+import product9 from "../assets/product9.jpg";
+import product10 from "../assets/product10.jpg";
+import product11 from "../assets/product11.jpg";
+import product12 from "../assets/product12.jpg";
 import { imageZoomEffect, TitleStyles } from "./ReusableStyles";
+import InfiniteScroll from "react-infinite-scroll-component";
+import React, { useCallback, useState } from "react";
+import Spinner from "./Spinner";
+
+
 export default function Products() {
   const data = [
     {
       image: product1,
       name: "Chicken Burger",
-      price: "$22.4/pcs",
+      price: "RS 22.4/pcs",
     },
     {
       image: product2,
       name: "Toasted Bread",
-      price: "$5.5/pcs",
+      price: "RS 5.5/pcs",
     },
     {
       image: product3,
       name: "Egg Sandwich",
-      price: "$8/pcs",
+      price: "RS 8/pcs",
     },
     {
       image: product4,
       name: "Raspberry Cake",
-      price: "$12.5/pcs",
+      price: "RS 12.5/pcs",
     },
     {
-      image: product1,
+      image: product5,
       name: "Chicken Burger",
-      price: "$22.4/pcs",
+      price: "RS 22.4/pcs",
     },
     {
-      image: product2,
+      image: product6,
       name: "Toasted Bread",
-      price: "$5.5/pcs",
+      price: "RS 5.5/pcs",
     },
     {
-      image: product3,
+      image: product7,
       name: "Egg Sandwich",
-      price: "$8/pcs",
-    },  
+      price: "RS 8/pcs",
+    },
     {
-      image: product4,
+      image: product8,
       name: "Raspberry Cake",
-      price: "$12.5/pcs",
+      price: "RS 12.5/pcs",
+    },
+    {
+      image: product9,
+      name: "Chicken Burger",
+      price: "RS 22.4/pcs",
+    },
+    {
+      image: product10,
+      name: "Toasted Bread",
+      price: "RS 5.5/pcs",
+    },
+    {
+      image: product11,
+      name: "Egg Sandwich",
+      price: "RS 8/pcs",
+    },
+    {
+      image: product12,
+      name: "Raspberry Cake",
+      price: "RS 12.5/pcs",
     }
   ];
+
+
+  const perPage = 4; 
+  const [lastPosition, setLastPosition] = useState(perPage); 
+  const [allPhotos, setAllPhotos] = useState(data.slice(0, perPage)); 
+  const [hasMore, setHasmore] = useState(true); 
+
+  const loadProducts = () => { 
+    setTimeout(() => { 
+      setAllPhotos((prev) => [...prev, ...data.slice(lastPosition, lastPosition + perPage)]
+    ); }, 4000); 
+    setLastPosition(lastPosition + perPage);
+    if (allPhotos.length === 12){
+      setHasmore(false) 
+    }
+  };
+
   return (
     <Section id="products">
       <div className="title">
@@ -55,21 +104,29 @@ export default function Products() {
           <span>Favourite</span> All the time!
         </h1>
       </div>
-      <div className="products">
-        {data.map((product) => {
-          return (
-            <div className="product">
-              <div className="image">
-                <img src={product.image} alt="Product" />
+      <InfiniteScroll
+        dataLength={allPhotos.length}
+        next={loadProducts}
+        hasMore={hasMore}
+        loader={<Spinner />}
+      >
+        
+        <div className="products">
+          {allPhotos.map((product, index) => {
+            return (
+              <div className="product" key={index}>
+                <div className="image">
+                  <img src={product.image} alt="Product" />
+                </div>
+                <h2>{product.name}</h2>
+                <h3>{product.price}</h3>
+                <p>He Printing and Typesetting the industry. Lorem Ipsum has</p>
+                <button>Buy Now</button>
               </div>
-              <h2>{product.name}</h2>
-              <h3>{product.price}</h3>
-              <p>He Printing and Typesetting the industry. Lorem Ipsum has</p>
-              {/* <button>Buy Now</button> */}
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </InfiniteScroll>
     </Section>
   );
 }
